9f159724fa831ebdb4d087cbfdd84062db3227bc
Mbotix Tech <143667670+MbotixTech@users.noreply.github.com>
2023-09-25 04:08:20 +0700

    Update crot.py

--- crot.py
+++ crot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = 'YOUR_TOKEN_BOT'
-YOUR_GROUP_ID = -ID_GROUP  # Ganti dengan ID grup/channel Anda
+YOUR_GROUP_ID = -ID_GROUP  # Yout group id
 
 def crot(update: Update, context):
     context.bot.forward_message(chat_id=YOUR_GROUP_ID, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
@@ -11,10 +11,8 @@ def main():
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Handlers
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, crot))
 
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
